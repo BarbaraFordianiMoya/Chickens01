8cd6017c7664f82a71556bb1c652608e33216b7c
Aluno <Aluno@10.85.198.10>
2024-06-12 14:53:09 -0300

    Chickens01

---
+++ src/Chickens01.java
@@ -0,0 +1,12 @@
+public class Chickens01 {
+    public static void main(String[] args) {
+        //Put yout code here
+        int x=5;
+        int y=3;
+        int v=y+1;
+        int z=((v)/2)*x;
+        int u=y*x;
+        int c=v*x;
+        System.out.println(z+u+c);
+    }   
+}
